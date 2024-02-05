d59aa8a4ccc658db941f1c2271675635af5436ef
ADITYA2777 <ajadityajain01@gmail.com>
2024-02-06 01:10:49 +0530

    project1 & project2

---
+++ digita_watch.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+>> commint
+   AuthorName: ADITYA JAIN
+   Project: DigitaWatch 
+   commint
+
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+     
+while true
+       echo -e $RED "DIGITAWATCH PROJECT 1"	
+do 
+	clear
+	echo -e $GREEN  $(date +%T)
+	sleep 1s
+
+done
